06469b2f5367bfe4045a10a1756c1b8eec22ad25
Тимофей <nestqi@mail.ru>
2024-09-26 22:20:47 +0300

    Добавьте файлы проекта.

---
+++ homework.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+#define task4
+
+#ifdef task1
+
+int main() {
+    float radius, height, volume, p;
+    scanf("%f %f", &radius, &height);
+    p = 3.1415;
+    volume = p * radius * radius * height;
+    printf("%.2f", volume);
+    return 0;
+}
+#endif task1 
+
+#ifdef task2
+int main() {
+    float radius, height, area, p;
+    scanf("%f %f", &radius, &height);
+    p = 3.14;
+    area = 2 * p * radius * (radius + height);
+    printf("%.2f\n", area);
+    return 0;
+}
+
+#endif task2
+
+#ifdef task3
+
+int main() {
+    int minutes, hours, remaining_minutes;
+    scanf("%d", &minutes);
+    hours = minutes / 60;
+    remaining_minutes = minutes % 60;
+    printf("%d min = %d hours and %d min", minutes, hours, remaining_minutes);
+    return 0;
+}
+
+#endif task3
+
+#ifdef task4
+
+#endif task4
+
+
