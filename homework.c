8b6763d9cc75e4bd5de5051ed162bd48700e4c1e
Тимофей <nestqi@mail.ru>
2024-10-11 09:26:05 +0300

    con1

--- homework.c
+++ homework.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 
-#define test
+#define contest1
 
 #ifdef test
 
@@ -15,3 +15,51 @@ int main()
 }
 #endif // test
 
+#ifdef task1
+
+int main()
+{
+    int arr[14], sum = 0, cnt = 0;
+
+    for (int i = 0; i < 14; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 14; i++)
+    {
+        if (arr[i] > 0 && arr[i] % 2 == 0)
+        {
+            sum += arr[i];
+            cnt += 1;
+        }
+    }
+    
+    printf("kol-vo %d\n", cnt);
+    printf("summa %d\n", sum);
+
+    return 0;
+}
+
+#endif // task1
+
+
+#ifdef contest1
+
+int main()
+{
+    int n = 0;
+    scanf("%d", &n);
+    int x[1000];
+    for (int i = 0; i < n; i++) {
+        int a = 0;
+        scanf("%d", &a);
+        x[i] = a;
+    }
+    for (int i = (n - 1); i >= 0; i--) {
+        int b = 0;
+        b = x[i] * x[i];
+        printf("%d\n", b);
+    }
+}
+
+#endif // contest1
